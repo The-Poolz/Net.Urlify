f69d3a63bbca8f30c6dda049a3fa20ddf7e650e6
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-06-04 20:34:15 +0300

    just copy-paste exist code (#1)

---
+++ src/Net.Urlify/Attributes/PropertyNameCasingAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Net.Urlify.Types;
+
+namespace Net.Urlify.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PropertyNameCasingAttribute : Attribute
+    {
+        public PropertyNameCasing Casing { get; }
+
+        public PropertyNameCasingAttribute(PropertyNameCasing casing = PropertyNameCasing.None)
+        {
+            Casing = casing;
+        }
+    }
+}
