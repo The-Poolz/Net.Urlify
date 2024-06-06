70a1b2479168229e5eacf0d27dec4e89934bb55e
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-06 15:45:40 +0300

    - code refactor

--- src/Net.Urlify/Extensions/UrlifyExtensions.cs
+++ src/Net.Urlify/Extensions/UrlifyExtensions.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
 using Net.Urlify.Models;
 using Net.Urlify.Attributes;
-using System.Collections.Generic;
 
 namespace Net.Urlify.Extensions
 {
@@ -11,23 +9,17 @@ namespace Net.Urlify.Extensions
     {
         internal static QueryStringParameterCollection ToQueryStringParameters(this Urlify sourceObject)
         {
-            var properties = sourceObject.GetType().GetProperties()
-                .Where(prop => Attribute.IsDefined(prop, typeof(QueryStringPropertyAttribute)));
-
-            var queryStringParams = new Dictionary<string, QueryStringParameterSettings>();
-            foreach (var property in properties)
-            {
-                var attribute = property.GetCustomAttribute<QueryStringPropertyAttribute>();
-                var value = property.GetValue(sourceObject);
-                if (value == null || attribute == null) continue;
-
-                var name = string.IsNullOrWhiteSpace(attribute.Name) ? property.Name : attribute.Name;
-                var isEncoded = attribute.IsEncoded;
-
-                queryStringParams.Add(name, new QueryStringParameterSettings(value.ToString(), isEncoded));
-            }
-
-            return new QueryStringParameterCollection(queryStringParams);
+            return new QueryStringParameterCollection(sourceObject.Properties
+                .Select(property => new {
+                    Property = property,
+                    Attribute = property.GetCustomAttribute<QueryStringPropertyAttribute>(),
+                    Value = property.GetValue(sourceObject)
+                })
+                .Where(x => x.Value != null && x.Attribute != null)
+                .ToDictionary(
+                    x => string.IsNullOrWhiteSpace(x.Attribute.Name) ? x.Property.Name : x.Attribute.Name,
+                    x => new QueryStringParameterSettings(x.Value.ToString(), x.Attribute.IsEncoded)
+                ));
         }
     }
 }
