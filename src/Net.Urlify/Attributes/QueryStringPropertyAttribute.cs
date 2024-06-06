b34e8df1ffdf9226aa47b1b864420a2871dfeddc
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-06 12:45:19 +0300

    Typo in `QueryStringPropertyAttribute.Name`

--- src/Net.Urlify/Attributes/QueryStringPropertyAttribute.cs
+++ src/Net.Urlify/Attributes/QueryStringPropertyAttribute.cs
@@ -5,7 +5,7 @@ namespace Net.Urlify.Attributes
     [AttributeUsage(AttributeTargets.Property)]
     public class QueryStringPropertyAttribute : Attribute
     {
-        public string? Name { get; }
+        public string Name { get; }
         public bool IsEncoded { get; }
 
         public QueryStringPropertyAttribute(string name = "", bool isEncoded = true)
